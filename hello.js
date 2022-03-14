0b905eba446fa5fd49a352aab4e0ef576b4d3f51
sri <sriyarocking98@gmail.com>
2022-03-14 13:17:05 +0530

    changed the endpoint

--- hello.js
+++ hello.js
@@ -7,7 +7,7 @@ const app = express();
 const port = 8080;
 const host = '0.0.0.0';
 
-app.get('/hello', (req, res) => {
+app.get('/', (req, res) => {
 	res.send('Hello, my name is Sriyancy');
 });
 app.listen(port, host);
